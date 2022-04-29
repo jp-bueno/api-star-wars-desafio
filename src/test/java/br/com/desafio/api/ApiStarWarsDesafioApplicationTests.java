2738cc796e6e29b98035b6a936c62db6e1d79fd7
João Pedro Bueno <joaopedrobueno2910@gmail.com>
2022-04-29 18:12:01 -0300

    Initial commit

---
+++ src/test/java/br/com/desafio/api/ApiStarWarsDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStarWarsDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
